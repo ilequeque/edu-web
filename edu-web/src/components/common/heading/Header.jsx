f23790a2f4eb3036a384864eae7aacd9e34fae00
ilequeque <ilyas.shakhabdinov@gmail.com>
2023-05-17 04:26:38 +0600

    header component

---
+++ edu-web/src/components/common/heading/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Head from "./Head"
+import "./header.css"
+
+export const Header = () => {
+  return (
+    <>
+        <Head />
+    </>
+  )
+}
+
+export default Header
