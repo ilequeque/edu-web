c68d9517daec491d41da1adb19a4c8fed86f26be
ilequeque <ilyas.shakhabdinov@gmail.com>
2023-05-21 06:35:28 +0600

    final pages

--- edu-web/src/App.js
+++ edu-web/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import "./App.css"
 import Header from "./components/common/header/Header"
-import Home from "./components/home/Home"
 import About from "./components/about/About"
 import CourseHome from "./components/allcourses/CourseHome"
+import Team from "./components/team/Team"
+import Pricing from "./components/pricing/Pricing"
 import Blog from "./components/blog/Blog"
 import Contact from "./components/contact/Contact"
+import Footer from "./components/common/footer/Footer"
+import Home from "./components/home/Home"
 
 import {
   BrowserRouter as Router,
@@ -19,12 +22,15 @@ const App = () => {
       <Router>
         <Header />
         <Switch>
-            <Route path='/' exact component={Home} />
-            <Route path='/about' exact component={About} />
-            <Route exact path='/courses' component={CourseHome} />
-            <Route exact path='/journal' component={Blog} />
-            <Route exact path='/contact' component={Contact} />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/courses' component={CourseHome} />
+          <Route exact path='/team' component={Team} />
+          <Route exact path='/pricing' component={Pricing} />
+          <Route exact path='/journal' component={Blog} />
+          <Route exact path='/contact' component={Contact} />
          </Switch>
+         <Footer />
       </Router>
     </>
   )
--- edu-web/src/components/home/Home.jsx
+++ edu-web/src/components/home/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Hero from './hero/Hero'
 import AboutCard from '../about/AboutCard'
+import Hblog from "./Hblog"
+import HAbout from "./HAbout"
+import Hprice from "./Hprice"
+import Testimonal from "./testimonal/Testimonal"
 
 const Home = () => {
   return (
     <>
         <Hero />
         <AboutCard />
+        <HAbout />
+        <Testimonal />
+        <Hblog />
+        <Hprice />
     </>
   )
 }
