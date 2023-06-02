327d850d499a8f470e6d1b346f655be74e7925e6
ilequeque <61391955+ilequeque@users.noreply.github.com>
2023-06-02 08:02:47 +0600

    hero component

--- edu-web/src/components/home/hero/Hero.jsx
+++ edu-web/src/components/home/hero/Hero.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import './hero.css';
 import Title from '../../common/title/Title';
-import { useHistory } from 'react-router-dom';
-import Login from '../../login/Login';
+import { Link } from 'react-router-dom';
 
 const Hero = () => {
-  const history = useHistory();
-
-  const handleClick = () => {
-    history.push(Login); // Replace '/login' with the actual login route path
-  };
   return (
     <>
         <section className='hero'>
@@ -17,14 +11,18 @@ const Hero = () => {
                 <div className="row">
                     <Title subtitle='Добро Пожаловать в ACADEMIA' title='Онлайн подготовка'/>
                     <p>ПОДГОТОВЬСЯ К ЕНТ ВМЕСТЕ С НАМИ!</p>
-                    <div className="button" onClick={handleClick}>
-                        <button className='primary-btn' onClick={handleClick}>
-                            НАЧАТЬ СЕЙЧАС <i className='fa fa-long-arrow-alt-right'></i>
-                        </button>
-                        <button>
-                            ПОСМОТРЕТЬ КУРСЫ <i className='fa fa-long-arrow-alt-right'></i>
-                        </button>
-                    </div>
+                    {/* <div className="button">
+                        <Link to='/login'>
+                            <button className='primary-btn'>
+                                НАЧАТЬ СЕЙЧАС <i className='fa fa-long-arrow-alt-right'></i>
+                            </button>
+                        </Link>
+                        <Link to='/courses'>
+                            <button>
+                                ПОСМОТРЕТЬ КУРСЫ <i className='fa fa-long-arrow-alt-right'></i>
+                            </button>
+                        </Link>
+                    </div> */}
                 </div>
             </div>
         </section>
@@ -33,4 +31,4 @@ const Hero = () => {
   )
 }
 
-export default Hero
+export default Hero;
